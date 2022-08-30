14bbb3e43b71c8ed3a770472e9a0cc948f6d15ab
Tony Tang <tsukiy0@users.noreply.github.com>
2022-08-30 03:57:58 +0000

    feat: impl guessing game

--- exercises/guessing_game/src/main.rs
+++ exercises/guessing_game/src/main.rs
@@ -1,4 +1,34 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
 fn main() {
+    let answer = rand::thread_rng().gen_range(1..=100);
+
+    println!("Answer is {answer}");
+
     println!("Guess the number!");
-    println!("Please input your guess:");
+
+    loop {
+        println!("Please input your guess:");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("error: failed to read line");
+
+        let guess: i32 = input.trim().parse().expect("error: input was not a number");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&answer) {
+            Ordering::Equal => {
+                println!("You win!");
+                break
+            },
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Less => println!("Too small!"),
+        }
+    }
 }
